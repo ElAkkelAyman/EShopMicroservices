3cbe40cf1fca6d45c8e996a8810dec88c13baef3
Mohamed Ayman EL AKKEL <maelakkel@sqli.com>
2024-06-03 10:33:32 +0100

    Add postgres docker image

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -8,7 +8,8 @@ namespace Catalog.API.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            app.MapPost("/products",
+                async (CreateProductRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CreateProductCommand>();
 
