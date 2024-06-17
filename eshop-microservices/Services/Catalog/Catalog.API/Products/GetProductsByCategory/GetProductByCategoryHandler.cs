0b5e8245d8f9d5b16c41e37c4e1b36399e82e239
Mohamed Ayman EL AKKEL <maelakkel@sqli.com>
2024-06-17 13:26:20 +0100

    Add get products by category and implement update logic

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Catalog.API.Products.GetProducts;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductsByCategory
+{
+    public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductQueryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GerPoductsByCategoryQueryHandler.handle called with {query}");
+            var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category)).ToListAsync(cancellationToken);
+            //products =  products.Where(p => p.Category.Contains(query.Category)).ToList();
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.GetProductsByCategory
+{
+    public record GetProductsByCategoryRequest(string Category);
+    public record GetProductsByCategoryResponse(IEnumerable<Product> Products);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{Category}", async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+
+                var response = result.Adapt<GetProductsByCategoryResponse>();
+
+                return Results.Ok(response);
+            })
+       .WithName("GetProductsByCategory")
+       .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+       .ProducesProblem(StatusCodes.Status400BadRequest)
+       .WithSummary("Get Product by Category")
+       .WithDescription("Get Product by Category");
+        }
+    }
+}
